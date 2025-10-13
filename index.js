67512b787104429e481c163171d2de6c885005f3
530hyelim <530hyelim@gmail.com>
2025-10-13 20:35:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Client, GatewayIntentBits, Partials, EmbedBuilder } from 'discord.js';
+import { Client, GatewayIntentBits, Partials, EmbedBuilder, ActivityType } from 'discord.js';
 import 'dotenv/config';
 
 const client = new Client({
